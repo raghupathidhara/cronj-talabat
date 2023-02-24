69c582aec39b99cdcd5b08451ad556bda124571c
raghupathidhara <raghupathi@cronj.com>
2023-02-24 16:41:42 +0530

    latest

---
+++ components/Approve/index.js
@@ -0,0 +1,29 @@
+import { Grid, Typography,Box,Button } from '@mui/material'
+import React from 'react'
+
+const Approve = ({setApprove}) => {
+  return (
+    <>
+    <Box className='flex flex-col m-3 bg-white'>
+      <Grid container>
+        <Grid item md={4}>
+            <Box className='border-2 border-lime-600 p-4 text-center bg-sky-50 rounded-md'>
+                <Typography className='font-bold text-emerald-400'>200.00 SAR</Typography>
+                <Typography className='font-bold text-neutral-400 text-sm'>Money on cashier</Typography>
+            </Box>
+        </Grid>
+      </Grid>
+      <Grid container className='flex items-center my-3'>
+            <Grid item md={2} >
+                <Button className='text-center text-xs font-bold w-full bg-rose-700 text-black py-4' variant='contained' onClick={()=>setApprove(true)}>Approve</Button>
+            </Grid>
+            <Grid item md={2}>
+            <Button className='text-center text-xs w-full bg-color-red text-white bg-slate-300 py-4' variant='contained'>Reject</Button>
+            </Grid>
+        </Grid>
+        </Box>
+    </>
+  )
+}
+
+export default Approve
--- components/Table/index.js
+++ components/Table/index.js
@@ -1,18 +1,68 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import React from "react";
 import Orders from '../Orders'
 
 function index({ table }) {
   return (
-    <Grid container>
+    <Grid container sx={{background:'#fff', padding:'1rem', borderRadius:'1rem'}}>
       {table && (
-        <Grid container>
-          <Grid item container>
-            {table.title}
+        <Grid container sx={{gap:'0.5rem'}}>
+          <Grid item container sx={{
+            alignItems:'center',
+            borderBottom:'1px solid #ccc'
+          }} >
+            <Grid item sx={{fontSize:'2rem', fontWeight:700, flex:1}}>
+                {table.title}   
+            </Grid>
+            <Grid item >
+                <Grid item container gap={'1rem'}>
+                <Button variant="contained" sx={{backgroundColor:"#C02328 !important"}}>Split Total Amount</Button>
+                <Button variant="contained" sx={{backgroundColor:"#C02328 !important"}}>Payment For Selected</Button>
+                <Button variant="contained" sx={{backgroundColor:"#DB4437 !important"}}>Not Paid</Button>
+                </Grid>
+            </Grid>
           </Grid>
           <Grid item container>
-            <Grid item container>Details</Grid>
-            {/* Details */}
+            <Grid item container sx={{fontSize:'1.4rem', color:'#C02328'}}>Details</Grid>
+            <Grid item container sx={{justifyContent:'space-between', alignItems:'center'}}>
+                <Grid item sx={{
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'center',
+                    gap:'0.5rem'
+                }}>
+                    <Grid item container sx={{color:"#A098AE"}}>Order Type</Grid>
+                    <Grid item container sx={{fontWeight:700}}>{table.details.orderType}</Grid>
+                </Grid>
+                <Grid item sx={{
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'center',
+                    gap:'0.5rem'
+                }}>
+                    <Grid item container sx={{color:"#A098AE"}}>Customer</Grid>
+                    <Grid item container sx={{fontWeight:700}}>{table.details.customer}</Grid>
+                </Grid>
+                <Grid item sx={{
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'center',
+                    gap:'0.5rem'
+                }}>
+                    <Grid item container sx={{color:"#A098AE"}}>Guests</Grid>
+                    <Grid item container sx={{fontWeight:700}}>{table.details.guests}</Grid>
+                </Grid>
+                <Grid item sx={{
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'center',
+                    gap:'0.5rem'
+                }}>
+                    <Grid item container sx={{color:"#A098AE"}}>Payment Method</Grid>
+                    <Grid item container sx={{fontWeight:700}}>{table.details.pyamentMethod ? table.details.pyamentMethod : "N/A"}</Grid>
+                </Grid>
+               
+            </Grid>
           </Grid>
           <Grid item container>
             <Grid item container>Orders</Grid>
