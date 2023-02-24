a6b014e74171289c0495f91d79c3938d5ebd038b
raghupathidhara <raghupathi@cronj.com>
2023-02-24 13:01:56 +0530

    font is added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
-import {Grid} from '@mui/material'
-
+import { Grid } from "@mui/material";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-  </h1>
-  )
+    <Grid container sx={{
+      gap:'2rem'
+    }}>
+      <Grid item container>
+        Header
+      </Grid>
+      <Grid
+        item
+        container
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "10% 1fr",
+          gap:'2rem'
+        }}
+      >
+        <Grid item>Sidebar</Grid>
+        <Grid item container>
+          <Grid item container>
+            cashier
+          </Grid>
+          <Grid item container sx={{
+            display:'grid',
+            gridTemplateColumns:'30% 1fr'
+          }} >
+            <Grid item container>Bills</Grid>
+            <Grid item container>Table 1</Grid>
+          </Grid>
+          <Grid item container>Footer</Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
