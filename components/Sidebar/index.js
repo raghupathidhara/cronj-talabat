412a73b713e697ff6be7c5e7d1e052d6ccaae115
sahil <sahil@cronj.com>
2023-02-24 13:01:50 +0530

    sidebar done

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,17 +1,44 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
+
+const menus = [
+  { icon: "/images/calendar.svg", label: "Orders" },
+  { icon: "/images/table.svg", label: "Tables" },
+  { icon: "/images/reservation.svg", label: "Reservation" },
+  { icon: "/images/sandclock.svg", label: "Waiting" },
+  { icon: "/images/calendar.svg", label: "Cashier" },
+];
 
 const Sidebar = () => {
+  const [selected,setselected]=useState(0)
   return (
-    <Box className="h-screen p-5 m-3 rounded-xl bg-zinc-400 w-max flex flex-col justify-between items-center">
-      <Box className="py-4 px-2">
+    <Box className="h-screen py-2 mx-4 my-2 rounded-xl bg-white w-max flex flex-col justify-between items-center w-24">
+      <Box className="py-2">
+        {menus.length &&
+          menus.map((menu,ind) => (
+            <Box className={`flex flex-col items-center cursor-pointer py-4 px-2 ${selected==ind?"border-solid border-r-2 border-red-700 box-border":"pr-2"}`} onClick={()=>setselected(ind)}>
+              <Box>
+                <img src={menu.icon} alt="menu"/>
+              </Box>
+              <Box>
+                <Typography variant="caption" className={`hover:text-red-600 ${selected==ind?"text-red-600":"text-stone-400"}`}>
+                  {menu.label}
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+      </Box>
+      <Box className="flex flex-col items-center cursor-pointer">
         <Box>
-            {/* <Box><img src={} alt="menu" /></Box> */}
-            <Box><Typography variant="caption" className="">Orders</Typography></Box>
+          <img src="/images/signOut.svg" alt="menu" />
+        </Box>
+        <Box>
+          <Typography variant="caption" className="text-stone-400">
+            Sign out
+          </Typography>
         </Box>
       </Box>
-      <Box>box2</Box>
     </Box>
   );
 };
