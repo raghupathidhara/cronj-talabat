2c92e4c424ce11c65f6f8b329dfcff7035fa22d0
raghupathidhara <raghupathi@cronj.com>
2023-02-24 14:18:00 +0530

    code is added

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -2,11 +2,11 @@ import { Grid } from "@mui/material";
 import { useState } from "react";
 import Bills from "../Bills";
 import Cashier from "../Cashier";
+import Table from '../Table'
 
 function index({ bills }) {
   const [selectedBill, setSelectedBill] = useState(0);
 
-  
 
   return (
     <Grid item container>
@@ -25,7 +25,7 @@ function index({ bills }) {
           <Bills bills={bills} setSelectedBill={setSelectedBill} />
         </Grid>
         <Grid item container>
-          Table 1
+          <Table table={bills[selectedBill]}/>
         </Grid>
       </Grid>
       <Grid item container>
